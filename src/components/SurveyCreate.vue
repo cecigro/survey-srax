ea9184c09e53e830ed6c84098065b6821fc134e6
Ceci <cecilia.guerrero@socialreality.com>
2020-03-26 16:28:18 -0700

    form reworked

--- src/components/SurveyCreate.vue
+++ src/components/SurveyCreate.vue
@@ -1,53 +1,129 @@
 <template>
-	<div>
-		<div style="margin-left: 50px;">Create a new survey</div>
-		<div v-for="(item, key) in items" :key="key" class="question-item">
-			<span>
-				Question {{ key + 1 }}
-				<el-button
-					v-if="key > 0"
-					type="text"
-					icon="el-icon-circle-close"
-					style="float: right;"
-					@click="deleteQuestion(key)">
-				</el-button>
-			</span>
-			<el-input placeholder="Write your question" v-model="item.question" size="mini">
-				<el-select v-model="item.type" slot="prepend" placeholder="Type of question" @change="handleTypeChange(key)">
-					<el-option label="Text" :value="1"/>
-					<el-option label="Multiple choice" :value="2"/>
-					<el-option label="Checkboxes" :value="3"/>
-					<el-option label="Dropdown" :value="4"/>
-				</el-select>
-			</el-input>
-			<div v-if="item.options.length" class="option-items">
-				<el-divider></el-divider>
-				<span>Options:</span>
-				<el-button
-					type="text"
-					icon="el-icon-circle-plus-outline"
-					style="float: right;"
-					@click="addOptions(key)"
-					size="mini">
-					Add Option
-				</el-button>
-				<br>
-				<el-input
-					size="mini"
-					v-for="(op, ix) in item.options"
-					:key="ix"
-					v-model="op.option"
-					:placeholder="`Option ${ ix + 1 }`">
-					<el-button icon="el-icon-delete" slot="append" @click="deleteOption(key,ix)"></el-button>
-				</el-input>
+	<div class="everything">
+
+		<el-row>
+			<el-col :span="24"><h2>Create a new survey</h2></el-col>
+		</el-row>
+
+		<el-row>
+			<el-col :span="24">
+				<div class="box-item">
+					Title: <el-input v-model="title" size="small" placeholder="Title of survey"/>
+				</div>
+			</el-col>
+		</el-row>
+		
+		<el-divider/>
+
+		<div class="page-items box-item" v-for="(page, key) in pages" :key="key">
+			<el-row>
+				<el-col :span="20">
+					<span class="title">Page {{ key + 1 }}</span>
+				</el-col>
+				<el-col :span="4">
+					<el-button
+						type="danger"
+						size="mini"
+						v-if="key > 0"
+						@click="deletePage(key)"
+						style="float: right;"
+						plain>Delete this page</el-button>
+				</el-col>
+			</el-row>
+			<el-row :gutter="20">
+				<el-col :span="12">
+					Title:
+					<el-input v-model="page.title" placeholder="Title of page"/>
+				</el-col>
+				<el-col :span="12">
+					Description:
+					<el-input type="textarea" rows="3" v-model="page.description"/>
+				</el-col>
+			</el-row>
+
+			<div v-for="(question, index) in page.questions" :key="index" class="question-item">
+
+				<el-row>
+					<el-col :span="24">
+						Question {{ index + 1 }}
+						<el-button
+						v-if="index > 0"
+						type="text"
+						icon="el-icon-circle-close"
+						style="float: right;"
+						@click="deleteQuestion(key, index)"/>
+					</el-col>
+				</el-row>
+
+				<el-row>
+					<el-col :span="24">
+						<el-input placeholder="Write your question" v-model="question.headings[0].heading">
+							<el-select
+								v-model="question.family"
+								slot="prepend"
+								placeholder="Type of question"
+								@change="handleTypeChange(index)">
+								<el-option label="Single choice" value="single_choice"/>
+								<el-option label="Multiple choice" value="multiple_choice"/>	
+								<el-option label="Matrix" value="matrix"/>	
+								<el-option label="Open ended" value="open_ended"/>	
+							</el-select>
+						</el-input>
+					</el-col>
+				</el-row>
+
+				<div class="answer-items" v-if="question.family">
+					<el-row>
+						<el-col :span="20">
+							Choices:
+						</el-col>
+						<el-col :span="4">
+							<el-button
+								type="text"
+								icon="el-icon-circle-plus-outline"
+								style="float: right;"
+								@click="addChoice(key, index)">
+									Add another choice
+							</el-button>
+						</el-col>
+					</el-row>
+					<el-row v-for="(choice, ix) in question.answers.choices" :key="ix">
+						<el-col :span="24">
+							<el-input v-model="choice.text" :placeholder="`Write choice ${choice.position}`">
+								<el-button
+									v-if="ix > 0"
+									icon="el-icon-delete"
+									slot="append"
+									@click="deleteChoice(key, index, ix)"
+								/>
+							</el-input>
+						</el-col>
+					</el-row>
+				</div>
+
+				<el-divider v-if="page.questions.length > 1"/>
+
 			</div>
+
+			<el-row style="margin-top: 10px;">
+				<el-col :span="24">
+					<el-button
+							type="primary"
+							@click="addQuestion(key)"
+							style="float: right;"
+							icon="el-icon-circle-plus-outline"
+							size="small">Add new question
+					</el-button>
+				</el-col>
+			</el-row>
 		</div>
-		<el-button
-				type="primary"
-				@click="addItem"
-				style="float: right; margin-right: 50px;"
-				icon="el-icon-circle-plus-outline"
-				size="mini">Add new question</el-button>
+
+		<el-row>
+			<el-col :span="24" style="text-align: center;">
+				<el-button type="primary" @click="addPage">Add Page</el-button>
+			</el-col>
+		</el-row>
+
 	</div>
 </template>
 <script>
@@ -55,55 +131,121 @@
 		name: 'survey-create',
 		data() {
 			return {
-				items: [
-					{ question: '', type: '', options: []}
+				title: '',
+				pages: [
+					{
+						title: '',
+						description: '',
+						position: 1,
+						questions: [
+							{
+								family: null,
+								subtype: 'vertical',
+								answers: {
+									choices: [
+										{ text: '', position: 1 },
+									],
+								},
+								headings: [
+									{ heading: '', position: 1 },
+								],
+							},
+						],
+					}
 				],
 			}
 		},
 		methods: {
-			addItem() {
-				this.items.push({ question: '', type: '', options: []});
+			addPage() {
+				this.pages.push({
+					title: '',
+					description: '',
+					position: 1,
+					questions: [
+						{
+							family: null,
+							subtype: 'vertical',
+							answers: {
+								choices: [
+									{ text: '', position: 1 },
+								],
+							},
+							headings: [
+								{ heading: '', position: 1 },
+							],
+						},
+					],
+				});
 			},
-			deleteQuestion(key) {
-				this.items.splice(key, 1);
+			deletePage(key) {
+				this.pages.splice(key, 1);
 			},
-			handleTypeChange(key) {
-				this.items[key].options = [];
-				if (this.items[key].type !== 1) {
-					this.items[key].options.push({option: ''});
-				}
+			addQuestion(key) {
+				this.pages[key].questions.push({
+					family: null,
+					subtype: 'vertical',
+					answers: {
+						choices: [
+							{ text: '', position: 1 },
+						],
+					},
+					headings: [
+						{ heading: '', position: 1 },
+					],
+				});
 			},
-			addOptions(index) {
-				this.items[index].options.push({option: ''});
+			deleteQuestion (key, index) {
+				this.pages[key].questions.splice(index, 1);
 			},
-			deleteOption(index, ix) {
-				this.items[index].options.splice(ix, 1);
-			}
+			addChoice(key, index) {
+				const position = this.pages[key].questions[index].answers.choices.length + 1;
+				this.pages[key].questions[index].answers.choices.push({ text: '', position: position });
+			},
+			deleteChoice (key, index, ix) {
+				this.pages[key].questions[index].answers.choices.splice(ix, 1);
+			},
 		}
 	}
 </script>
 <style lang="scss">
-	.question-item {
-		display: block;
-    position: relative;
-    background: #f6fbff;
-    margin: 10px 50px;
-    border: 1px solid #bfe3ff;
-    border-radius: 7px;
-    padding:10px;
-		>span {
+	.everything {
+		margin: 0 150px;
+		.box-item {
 			display: block;
 			position: relative;
-		}
-		>.el-input {
-			.el-input-group__prepend {
-				width: 100px;
+			background: #f6fbff;
+			border: 1px solid #bfe3ff;
+			border-radius: 7px;
+			padding:10px;
+			.title{
+				display: block;
+				font-weight: bold;
+				margin-bottom: 10px;
+			}
+			.question-item {
+				.el-row{
+					.el-col{
+						.el-input {
+							.el-input-group__prepend {
+								width: 110px;
+							}
+						}
+					}
+				}
+				.answer-items {
+					.el-row {
+						.el-col {
+							.el-input {
+								margin-top: 10px;
+							}
+						}
+					}
+				}
 			}
 		}
-		.option-items {
-			>.el-input {
-				margin: 5px 0;
-			}
+		.page-items {
+			display: block;
+			margin: 15px 0;
 		}
 	}
 </style>
